25fa09f72955648db025c340aea12e7b8032f7e9
Leonardo Thibau <leonardo_thibau@hotmail.com>
2025-03-18 18:14:00 -0300

    melhor versao pluscode+end

--- Lojas/Juntando Tudo/Código bom NA.R
+++ Lojas/Juntando Tudo/Código bom NA.R
@@ -1,4 +1,4 @@
-#  carregar pacotes
+# Carregar pacotes
 library(tidyverse)
 library(readxl)
 library(writexl)
@@ -42,47 +42,67 @@ extrair_endereco <- function(endereco) {
   )
 }
 
+# Função para extrair bairro e cidade do Plus Code
+extrair_bairro_cidade <- function(plus_code) {
+  # Regex para capturar o bairro e a cidade
+  padrao <- "[A-Z0-9+]+\\s([^,]+),\\s([^-]+)\\s-\\s[A-Z]{2}"
+  
+  # Extrair bairro e cidade
+  bairro <- str_match(plus_code, padrao)[,2]
+  cidade <- str_match(plus_code, padrao)[,3]
+  
+  # Retornar como uma lista
+  return(list(bairro = bairro, cidade = cidade))
+}
+
+# Função para transformar em "NA" se começar com termos específicos
+transformar_para_NA <- function(texto) {
+  # Verificar se o valor é NA
+  if (is.na(texto)) {
+    return("NA")
+  }
+  
+  # Lista de termos que devem ser transformados em "NA"
+  termos_na <- c("^r\\.", "^rua", "^av\\.", "^aven\\.", "^avenida", "^s/n", "^sn", "^na")
+  
+  # Verificar se o texto começa com algum dos termos (ignorando maiúsculas/minúsculas)
+  if (any(str_detect(str_to_lower(texto), termos_na))) {
+    return("NA")
+  }
+  
+  return(texto)
+}
+
 # Aplicar a função e criar as novas colunas
 dados <- dados %>%
   bind_cols(map_dfr(dados$Endereço, extrair_endereco))
 
-# Verificar e corrigir NAs usando o segundo código
+# Verificar se as colunas "Bairro" e "Cidade" existem
+if (!"Bairro" %in% colnames(dados)) {
+  stop("A coluna 'Bairro' não foi criada corretamente.")
+}
+if (!"Cidade" %in% colnames(dados)) {
+  stop("A coluna 'Cidade' não foi criada corretamente.")
+}
+
+# Aplicar a função transformar_para_NA às colunas Bairro e Cidade
 dados <- dados %>%
   rowwise() %>%
   mutate(
-    Bairro = ifelse(is.na(Bairro) || Bairro == "NA", extrair_bairro_cidade(Endereço)$bairro, Bairro),
-    Cidade = ifelse(is.na(Cidade) || Cidade == "NA", extrair_bairro_cidade(Endereço)$cidade, Cidade)
+    Bairro = transformar_para_NA(Bairro),
+    Cidade = transformar_para_NA(Cidade)
+  )
+
+# Verificar e corrigir NAs usando o Plus Code
+dados <- dados %>%
+  rowwise() %>%
+  mutate(
+    Bairro = ifelse(is.na(Bairro) || Bairro == "NA", extrair_bairro_cidade(`Plus_Code`)$bairro, Bairro),
+    Cidade = ifelse(is.na(Cidade) || Cidade == "NA", extrair_bairro_cidade(`Plus_Code`)$cidade, Cidade)
   )
 
 # Salvar o resultado em uma nova planilha
 write_xlsx(dados, "C:/Users/leona/Github/Web-Scraping-Maps-Rstudio/Lojas/Enderecos_Processados.xlsx")
-
-transformar_bairro <- function(bairro) {
-  # Verificar se o bairro é vazio ou não é uma string
-  if (is.null(bairro) || bairro == "" || !is.character(bairro)) {
-    return("NA")
-  }
-  
-  # Verificar se o texto contém números, hífen, "BA" ou uma letra isolada
-  if (grepl("\\d|[-]|\\bBA\\b|\\b\\w\\b", bairro)) {
-    return("NA")
-  }
-  
-  return(bairro)
-}
-
-# Teste
-bairro <- "bairro"
-texto_bairro <- "BA"
-print(transformar_bairro(texto_bairro))  # Saída: "NA"
-
-
-# Carregar pacotes necessários
-library(readxl)
-library(openxlsx)
-
-# Carregar a planilha
-df <- read_excel("C:/Users/leona/Github/Web-Scraping-Maps-Rstudio/Lojas/Enderecos_Processados.xlsx")
 
 # Função para modificar os bairros
 modificar_bairro <- function(bairro) {
@@ -92,6 +112,22 @@ modificar_bairro <- function(bairro) {
   return(bairro)
 }
 
+# Função para transformar cidade
+transformar_cidade <- function(cidade) {
+  # Verificar se a cidade é vazia ou não é uma string
+  if (is.null(cidade) || cidade == "" || !is.character(cidade)) {
+    return("NA")
+  }
+  
+  # Verificar se a cidade contém "S/N", números, hífen ou pontuação
+  if (grepl("S/N|\\d|[-]|[[:punct:]]", cidade)) {
+    return("NA")
+  }
+  
+  return(cidade)
+}
+
+# Carregar a planilha
 df_lojas <- read_excel("C:/Users/leona/Github/Web-Scraping-Maps-Rstudio/Lojas/Enderecos_Processados.xlsx")
 
 # Aplicar a função à coluna "Bairro"
@@ -99,4 +135,4 @@ df_lojas$Bairro <- sapply(df_lojas$Bairro, modificar_bairro)
 
 # Salvar a planilha modificada
 write.xlsx(df_lojas, "C:/Users/leona/Github/Web-Scraping-Maps-Rstudio/Lojas/Enderecos_Processados.xlsx", rowNames = FALSE)
-
+         
